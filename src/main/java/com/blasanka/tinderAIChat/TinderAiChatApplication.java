ee359c91dabc9eacf02ef41850b51b58fc39b821
blasanka <blasanka95@gmail.com>
2024-09-15 10:17:16 +0530

    Init

---
+++ src/main/java/com/blasanka/tinderAIChat/TinderAiChatApplication.java
@@ -0,0 +1,13 @@
+package com.blasanka.tinderAIChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderAiChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderAiChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blasanka/tinderAIChat/TinderAiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blasanka.tinderAIChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderAiChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
